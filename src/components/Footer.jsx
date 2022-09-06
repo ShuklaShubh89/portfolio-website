4fc11ccf83825e865d29f95772624814432951f4
Shubham Shukla <shubham.cored@gmail.com>
2022-09-07 01:54:32 +0530

    feat: updating the counter logic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,60 +4,31 @@ import Container from "react-bootstrap/Container";
 const Footer = (props) => {
   const bgStyle = { backgroundColor: "#f5f5f5" };
 
-  const counter = document.querySelectorAll('#counter span');
 
-  const setValue = (num) => {
-    var str = num.toString().padStart(6, "0");
-    for (let index = 0; index < str.length; index++) {
-      const element = str[index];
-      counter[index].innerHTML = element;
-    }
-  };
-  
-  var countvar=0
-  
-  const readPayload = {
-      "operation": "list",
-      "tableName": "countervisitor",
-      "select": "COUNT",
-      "payload": {
-          "TableName": "countervisitor",
-          "Select": "COUNT"    
+  React.useEffect(() => {
+      const counter = document.querySelectorAll('#counter span');
+
+      const setValue = (num) => {
+        var str = num.toString().padStart(6, "0");
+        for (let index = 0; index < str.length; index++) {
+          const element = str[index];
+          counter[index].innerHTML = element;
         }
-  }
-  
-  var updatePayload = {
-    "operation": "update",
-    "tableName": "countervisitor",
-    "payload": {
-      "Key": {
-        "count": countvar
+      };
+      
+      var countvar=0
+      
+      const readPayload = {
+          "operation": "list",
+          "tableName": "countervisitor",
+          "select": "COUNT",
+          "payload": {
+              "TableName": "countervisitor",
+              "Select": "COUNT"    
+            }
       }
-    }
-  }
-  
-  const readAction = async(flag) => {
-    const response = await fetch('https://j9t296xsr2.execute-api.us-east-1.amazonaws.com/prod/ddmanager', {
-      method: 'POST',
-      body: JSON.stringify(readPayload),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const myJson = await response.json(); //extract JSON from the http response
-    countvar = myJson.Count;
-    if(flag=="new"){
-      writeAction()
-    }else {
-      setValue(countvar)
-    }
-  }
-  
-  const writeAction = async() => {
-    countvar+=1
-    const response = await fetch('https://j9t296xsr2.execute-api.us-east-1.amazonaws.com/prod/ddmanager', {
-      method: 'POST',
-      body: JSON.stringify({
+      
+      var updatePayload = {
         "operation": "update",
         "tableName": "countervisitor",
         "payload": {
@@ -65,16 +36,46 @@ const Footer = (props) => {
             "count": countvar
           }
         }
-      }),
-      headers: {
-        'Content-Type': 'application/json'
       }
-    });
-    const myJson = await response.json();
-    setValue(countvar)
-  }
-
-  React.useEffect(() => {
+      
+      const readAction = async(flag) => {
+        const response = await fetch('https://j9t296xsr2.execute-api.us-east-1.amazonaws.com/prod/ddmanager', {
+          method: 'POST',
+          body: JSON.stringify(readPayload),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const myJson = await response.json(); //extract JSON from the http response
+        countvar = myJson.Count;
+        if(flag=="new"){
+          writeAction()
+        }else {
+          setValue(countvar)
+        }
+      }
+      
+      const writeAction = async() => {
+        countvar+=1
+        const response = await fetch('https://j9t296xsr2.execute-api.us-east-1.amazonaws.com/prod/ddmanager', {
+          method: 'POST',
+          body: JSON.stringify({
+            "operation": "update",
+            "tableName": "countervisitor",
+            "payload": {
+              "Key": {
+                "count": countvar
+              }
+            }
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const myJson = await response.json();
+        setValue(countvar)
+      }
+    
       if (localStorage.getItem("hasVisited") == null) {
       console.log('this is here')
       readAction("new")
